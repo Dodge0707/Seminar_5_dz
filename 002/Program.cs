e4a5bdc140b9ab45f5f18961ca986f27398da3b6
«Vova <vrobskiy@gmail.com>
2022-04-28 22:15:38 +0300

    add example 002

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,23 +1,27 @@
-﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
 
 Console.Write("Введите количество элементов массива -> ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int[] numbers = new int[number];
 
+
+
 FillArray(numbers);
 WriteArray(numbers);
-int result = Kolvo(numbers);
+int result = Summ(numbers);
 Console.WriteLine(result);
 
-int Kolvo(int[] array)
+
+int Summ(int[] array)
 {
     int result = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 0)
+        if (array[i] % 2 > 0 || array[i] % 2 < 0)
         {
-            result++;
+            result += array[i];
         }
     }
     return result;
@@ -29,7 +33,7 @@ void FillArray(int[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(100, 1000);
+        numbers[i] = new Random().Next(-100, 101);
     }
 }
 
